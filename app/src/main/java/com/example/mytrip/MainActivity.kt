4cebcc9d5f9cc24a5008a66f29f579180b108b9a
jorgetranin <antoniotraninjorge@gmail.com>
2022-11-13 10:48:39 -0400

    Implementação do calculo e return para a apresentação

--- app/src/main/java/com/example/mytrip/MainActivity.kt
+++ app/src/main/java/com/example/mytrip/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.mytrip
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -12,19 +11,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Mapeamento dos elementos da minnha tela
+        // Mapeamento dos elementos da minha tela
         val txt_Distancia = findViewById<EditText>(R.id.txt_Distancia)
         val txt_preco = findViewById<EditText>(R.id.txt_preco)
         val txt_autonomia = findViewById<EditText>(R.id.txt_autonomia)
         val txt_Resultado = findViewById<TextView>(R.id.txt_Resultado)
         val btn_Calcular = findViewById<Button>(R.id.btn_Calcular)
 
+
         // Função para escutar o evento de click no button
 
+        if (txt_Distancia.text.isBlank()) {
+            txt_Distancia.error = ("Digite a Distancia! ")
+        }
+
+        if (txt_preco.text.isBlank()) {
+            // se o usuario nao digitar nada aparecera esta mensagem de erro
+            txt_preco.error = ("Digite o preço! ")
+        }
+
+        if (txt_autonomia.text.isBlank()) {
+            // se o usuario nao digitar nada aparecera esta mensagem de erro
+            txt_autonomia.error = ("Qual a autonomia! ")
+        }
+
+        btn_Calcular.setOnClickListener {
+
+            //calculo é Distancia*preço/autonomida
+            val result = (txt_Distancia.text.toString().toFloat() * txt_preco.text.toString()
+                .toFloat()) / txt_autonomia.text.toString().toFloat()
+            // Apresentação do resultado no textView
+            txt_Resultado.text = result.toString()
+        }
 
     }
 
-    fun btnCalcular(view: View) {
 
-    }
 }
